5f77b16dd30e86f565f558b465ca50bff7b10500
Harshada-Magdum <147029377+Harshada-Magdum@users.noreply.github.com>
2025-08-20 18:01:24 +0530

    Updated the scripts

---
+++ generated-tests/generated-tests/src/test/java/pages/CartPage.java
@@ -0,0 +1,22 @@
+## TestScripts file=generated-tests/src/test/java/pages/CartPage.java type=REFERENCE
+package pages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+public class CartPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    // Placeholder locator for Checkout button on Cart page
+    private By checkoutButton = By.id("checkout");
+    public CartPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+    public void ClickIcon() {
+        WebElement checkoutBtn = wait.until(ExpectedConditions.elementToBeClickable(checkoutButton));
+        checkoutBtn.click();
+    }
+}
